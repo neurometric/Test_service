94df5b3e019be7c7612a97a51f9d19b4563c6dc1
Khamza nabiev <hamza-nabiyev88@mail.ru>
2025-05-23 11:11:39 +0300

    fixed file Product.java from GenerationType.IDENTITY to GenerationType.AUTO

--- backend/src/main/java/com/yandex/practicum/devops/model/Product.java
+++ backend/src/main/java/com/yandex/practicum/devops/model/Product.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.NotNull;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @NotNull(message = "Product name is required.")
